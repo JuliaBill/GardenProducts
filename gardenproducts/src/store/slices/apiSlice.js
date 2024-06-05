f1df077a9bd40b6874be17b1ffbbeab9fcf9ea8b
JuliaMiller <julia1612@freenet.de>
2024-06-05 11:43:16 +0200

    second commit

---
+++ gardenproducts/src/store/slices/apiSlice.js
@@ -0,0 +1,36 @@
+// src/store/api/apiSlice.js
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../../config";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    fetchCategories: builder.query({
+      query: () => "categories/all",
+    }),
+    fetchCategoryByName: builder.query({
+      query: (name) => `categories/${name}`,
+    }),
+    fetchAllProducts: builder.query({
+      query: () => "products/all",
+    }),
+
+    postDiscount: builder.mutation({
+      query: (discountData) => ({
+        url: "sale/send",
+        method: "POST",
+        body: discountData,
+      }),
+    }),
+  }),
+});
+
+
+export const {
+  useFetchCategoriesQuery,
+  useFetchCategoryByNameQuery,
+  useFetchAllProductsQuery,
+  useFetchProductByIdQuery,
+  usePostDiscountMutation,
+} = apiSlice;
