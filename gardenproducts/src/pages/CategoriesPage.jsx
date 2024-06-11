2d596f9511f0fabe22540ecfde9762b6e65e77a6
JuliaMiller <julia1612@freenet.de>
2024-06-11 11:48:50 +0200

    added CategoriesPage

--- gardenproducts/src/pages/CategoriesPage.jsx
+++ gardenproducts/src/pages/CategoriesPage.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
 
+import CategoryContainer from '../components/CategoriesComponents/categoryContainer/CategoryContainer'
+
+export default function CategoriesPage() {
+  return (
+    <section className={`container`}>
+     
+      <div className="grid">
+        <h2 className="grid__title">Categories</h2>
+        <div>
+          <CategoryContainer limitDisplay={false} />
+        </div>
+      </div>
+    </section>
+  )
+}
