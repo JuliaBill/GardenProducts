af1262eae0cd63db0ee7eedc3c339ae745b060e8
JuliaMiller <julia1612@freenet.de>
2024-07-05 10:24:52 +0200

    bugs fixed

--- gardenproducts/src/Layout/Header/Header.jsx
+++ gardenproducts/src/Layout/Header/Header.jsx
@@ -15,9 +15,9 @@ import shoppingBagWhite from '../../media/icons/shoppingBagWhite.svg'
 import heart from '../../media/icons/heart.svg'
 import heartWhite from '../../media/icons/heartWhite.svg'
 
+import DiscountPopUp from '../../components/DiscountPopUp/DiscountPopUp'
 import DiscountButton from '../../components/DiscountButton/DiscountButton'
 import BurgerMenu from './../../components/BurgerMenu/BurgerMenu'
-import DiscountPopUp from '../../components/DiscountPopUp/DiscountPopUp'
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
